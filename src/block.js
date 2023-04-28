836a62952ad3a000a7fca895e68381b0e7766e57
Giancarlo Sanchez <gianksp@gmail.com>
2023-04-28 09:49:27 +0200

    Modernized

--- src/block.js
+++ src/block.js
@@ -5,10 +5,10 @@ const svg = `
 const block = {
   id: `section-${process.env.MODULE_ID}`,
   media: `
-    <button type="button" class="text-gray-900 hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-3 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2">
+    <a type="button" class="text-gray-900 hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-3 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2">
       ${svg}
       <span style="min-width: 110px">Connect Wallet</span>
-    </button> 
+    </a> 
   `,
   category: "Login",
   label: "Login with Metamask",
